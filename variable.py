42e60561c2bc0ff91394d06758cb35669dd5908a
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-06-19 09:21:51 +0530

    Day 02: Variables in Python

---
+++ variable.py
@@ -0,0 +1,53 @@
+# VARIABLE DAY 2 
+
+
+# VARIABLE -> variable ek container ki taraha hot hi jisme hum data store krte hai 
+# eg. 1] Ek bottle mein paani store karte hai
+         # ab isme botte jo hai wo ek variable hai aur jo paani hai wo data hai , jo variable (bottle) mai save hai .
+#.    2] Ek box mein books rakhte ho
+         # box(variable)
+         # books(data)
+
+# points
+#  
+# . Variable mein numbers, text, ya koi bhi value store kar sakte hain.
+# eg. 
+
+name = "Sumit"
+age = 21
+print("sumit")
+
+# name , age  jo hai wo variable hai aur sumit , 21 cler data hai
+
+
+name = "Sumit" # string
+age = 21 # integer
+price = 99.99 # float
+is_student = True  # Boolean(true/false)
+
+# Variable naming rule 
+
+#✅
+name = "Sumit"
+user_name = "Sumit"
+age1 = 21
+marks = 90
+
+#❎
+#.   1name = "Sumit"     # Number se start nahi kar sakte
+#.   user-name = "Sumit" # Hyphen allowed nahi hai
+#.   class = "Python"    # Reserved keyword hai
+
+
+
+#solve this code (Don't just copy paste)
+
+#1]
+
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+
+
+# Variable ek naam diya hua container hota hai jisme hum data store karte hain aur zarurat padne par us data ko use ya change kar sakte hain.
